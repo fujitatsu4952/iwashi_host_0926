d4a9095b1d3297286f3c11c8e14698df89da1e82
fujitatsu4952 <fujitatsu49528@gmail.com>
2020-10-24 23:37:35 +0900

    ad

---
+++ abr/index.ts
@@ -0,0 +1,9 @@
+import {
+    GuestRoomInteractor,
+    GuestPolicyInteractor,
+    GuestPlanInteractor
+} from 'iwashi_abr_1023/iwashiabr';
+
+export const roomInteractor = new GuestRoomInteractor();
+export const policyInteractor = new GuestPolicyInteractor();
+export const planInteractor = new GuestPlanInteractor();
